81d68bb284ce9b133ef5d736d049d9649f6cec5d
Nico Rieck <nico.rieck@gmail.com>
2018-03-06 23:21:40 +0100

    EMC: Remove dead code

---
+++ src/EventManifestCompiler/ConsoleDiagnosticPrinter.cs
@@ -0,0 +1,61 @@
+namespace EventManifestCompiler
+{
+    using System;
+    using System.IO;
+    using EventManifestCompiler.Support;
+    using EventManifestFramework.Support;
+
+    /// <summary>
+    ///   Diagnostic consumer that prints all diagnostics to the console.
+    /// </summary>
+    internal sealed class ConsoleDiagnosticPrinter : IDiagnosticConsumer
+    {
+        private readonly TextWriter output;
+
+        public ConsoleDiagnosticPrinter(TextWriter output)
+        {
+            this.output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        public void HandleDiagnostic(
+            DiagnosticSeverity severity, SourceLocation location, string message)
+        {
+            if (!Enum.IsDefined(typeof(DiagnosticSeverity), severity))
+                throw new ArgumentOutOfRangeException(nameof(severity), severity, null);
+            if (location == null)
+                throw new ArgumentNullException(nameof(location));
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+
+            if (severity == DiagnosticSeverity.Ignored)
+                return;
+
+            using (new ConsoleColorScope()) {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                if (location.FilePath != null) {
+                    output.Write(location.FilePath);
+                    if (location.LineNumber != -1) {
+                        if (location.ColumnNumber != -1)
+                            output.Write("({0},{1})", location.LineNumber, location.ColumnNumber);
+                        else
+                            output.Write("({0})", location.LineNumber);
+                    }
+                    output.Write(": ");
+                }
+                if (severity == DiagnosticSeverity.Note) {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    output.Write("note: ");
+                } else if (severity == DiagnosticSeverity.Warning) {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    output.Write("warning: ");
+                } else if (severity == DiagnosticSeverity.Error) {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    output.Write("error: ");
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                output.WriteLine(message);
+            }
+        }
+    }
+}
--- src/EventManifestCompiler/Extensions/XmlExtensions.cs
+++ src/EventManifestCompiler/Extensions/XmlExtensions.cs
@@ -2,16 +2,9 @@ namespace EventManifestCompiler.Extensions
 {
     using System.Xml;
     using System.Xml.Linq;
-    using System.Xml.Schema;
 
     internal static class XmlExtensions
     {
-        public static void Add(
-            this XmlSchemaSet set, XNamespace targetNamespace, string schemaUri)
-        {
-            set.Add(targetNamespace.NamespaceName, schemaUri);
-        }
-
         public static void AddNamespace(
             this XmlNamespaceManager nsmgr, string prefix, XNamespace ns)
         {
