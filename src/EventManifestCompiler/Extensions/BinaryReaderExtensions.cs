826882060fcb00fa4ca70645b4243973ea5a5fbe
Nico Rieck <nico.rieck@gmail.com>
2018-03-07 00:10:17 +0100

    {fixup} binxml cleanup

--- src/EventManifestCompiler/Extensions/BinaryReaderExtensions.cs
+++ src/EventManifestCompiler/Extensions/BinaryReaderExtensions.cs
@@ -4,16 +4,9 @@ namespace EventManifestCompiler.Extensions
     using System.IO;
     using System.Text;
 
-    static class BinaryReaderExtensions
+    internal static class BinaryReaderExtensions
     {
-        public static string ReadPaddedString(this BinaryReader reader, Encoding encoding, int byteCount)
-        {
-            var bytes = new byte[byteCount];
-            reader.Read(bytes, 0, bytes.Length);
-            return encoding.GetString(bytes).TrimEnd('\0');
-        }
-
-        public static string ReadPaddedString(this BinaryReader reader, int byteCount)
+        public static string ReadPaddedString(this BinaryReader reader, uint byteCount)
         {
             return reader.ReadPaddedString(Encoding.ASCII, byteCount);
         }
@@ -23,11 +16,6 @@ namespace EventManifestCompiler.Extensions
             var bytes = new byte[byteCount];
             reader.Read(bytes, 0, bytes.Length);
             return encoding.GetString(bytes).TrimEnd('\0');
-        }
-
-        public static string ReadPaddedString(this BinaryReader reader, uint byteCount)
-        {
-            return reader.ReadPaddedString(Encoding.ASCII, byteCount);
         }
 
         public static Guid ReadGuid(this BinaryReader r)
