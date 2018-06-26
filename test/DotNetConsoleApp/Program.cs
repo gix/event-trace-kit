5712027a21b61588e21d423918b0b20223b08a80
Nico Rieck <nico.rieck@gmail.com>
2018-06-26 04:32:56 +0200

    Add managed event logging to sample test

--- test/DotNetConsoleApp/Program.cs
+++ test/DotNetConsoleApp/Program.cs
@@ -1,16 +1,29 @@
 namespace ConsoleApp
 {
     using System;
+    using System.Diagnostics.Tracing;
     using System.Threading;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            var es = new MinimalEventSource();
             for (int i = 0; i < 50; ++i) {
-                Console.WriteLine("ConsoleApp {0}", i);
+                es.EventWrite(i);
+                Console.WriteLine("DotNetConsoleApp {0}", i);
                 Thread.Sleep(1000);
             }
+        }
+    }
+
+    [EventSource(Guid = "{5AB0948E-C045-411A-AC12-AC455AFA8DF2}")]
+    internal sealed class MinimalEventSource : EventSource
+    {
+        [Event(1)]
+        public void EventWrite(int id)
+        {
+            WriteEvent(1, id);
         }
     }
 }
