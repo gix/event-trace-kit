2527c9e7dde0365f3ddc2c78b294326b12c26bb7
Nico Rieck <nico.rieck@gmail.com>
2018-06-28 03:11:28 +0200

    Update build

--- src/EventTraceKit.VsExtension/GlobalSuppressions.cs
+++ src/EventTraceKit.VsExtension/GlobalSuppressions.cs
@@ -1,1 +1,2 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "VSTHRD200:Use \"Async\" suffix for async methods", Justification = "Noise")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD010:Invoke single-threaded types on Main thread", Justification = "Noise")]
