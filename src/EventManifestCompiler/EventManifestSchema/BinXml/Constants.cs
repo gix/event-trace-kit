79065c3052bac272ac8d51a5a3c0e60ab5931a1a
Nico Rieck <nico.rieck@gmail.com>
2019-02-03 20:52:42 +0100

    Remove unused files

--- src/EventManifestCompiler/EventManifestSchema/BinXml/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace InstrManifestCompiler.EventManifestSchema.BinXml
-{
-    internal static class Constants
-    {
-        public const byte MajorVersion = 1;
-        public const byte MinorVersion = 1;
-        public const byte HasMoreFlag = 0x40;
-    }
-}
